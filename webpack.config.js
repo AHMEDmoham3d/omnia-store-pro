ddd02816250c74b4057f6066d440613bcbe38d0c
Ahmed Mohamed <162385026+AHMEDmoham3d@users.noreply.github.com>
2026-02-24 14:15:20 +0200

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,75 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true,
-    publicPath: '/',
-  },
-  mode: 'production',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      inject: 'body',
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.tsx',
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProduction
+        ? 'bundle.[contenthash].js'
+        : 'bundle.js',
+      clean: true,
+      publicPath: '/',
     },
-    historyApiFallback: true,
-    compress: true,
-    port: 3000,
-    hot: true,
-  },
+
+    mode: isProduction ? 'production' : 'development',
+
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-react',
+                '@babel/preset-typescript'
+              ],
+            },
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        inject: 'body',
+      }),
+
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(
+          isProduction ? 'production' : 'development'
+        ),
+      }),
+    ],
+
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+
+    devServer: {
+      historyApiFallback: true,
+      compress: true,
+      port: 3000,
+      hot: true,
+      open: true,
+    },
+  };
 };
