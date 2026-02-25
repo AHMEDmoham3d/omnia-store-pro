fb80ef276908451125c7f27b7c57fc4d72b7a465
Ahmed Mohamed <162385026+AHMEDmoham3d@users.noreply.github.com>
2026-02-25 20:38:46 +0200

    first commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,17 @@
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = 'https://tixxvcxcrgxscmprldmi.supabase.co';
+// const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpeHh2Y3hjcmd4c2NtcHJsZG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NzA5MzksImV4cCI6MjA2OTA0NjkzOX0.bhWFkJAMPAnEf9c1rRjEbyYG4XjQnOIP2dsVVeK_H3U';
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tixxvcxcrgxscmprldmi.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpeHh2Y3hjcmd4c2NtcHJsZG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NzA5MzksImV4cCI6MjA2OTA0NjkzOX0.bhWFkJAMPAnEf9c1rRjEbyYG4XjQnOIP2dsVVeK_H3U';
+const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY as string;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase env variables are missing');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
