1a022e44b3e1225c89ae7adfb72d33a518c8d7e2
Ahmed Mohamed <162385026+AHMEDmoham3d@users.noreply.github.com>
2026-02-26 13:49:58 +0200

    first commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,13 +5,9 @@
 
 // export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = (import.meta as any).env.VITE_SUPABASE_URL ?? '';
-const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY ?? '';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || '';
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase env variables are missing');
+  console.log("ENV ERROR:", supabaseUrl, supabaseAnonKey);
 }
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
