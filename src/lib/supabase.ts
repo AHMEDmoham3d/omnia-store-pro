75843d80b77ac20675bd93785e861dda1ab7a3b6
Ahmed Mohamed <162385026+AHMEDmoham3d@users.noreply.github.com>
2026-03-14 14:36:39 +0200

    force rebuild

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -31,6 +31,6 @@ const supabaseUrl = "https://tixxvcxcrgxscmprldmi.supabase.co";
 const supabaseKey =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpeHh2Y3hjcmd4c2NtcHJsZG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM0NzA5MzksImV4cCI6MjA2OTA0NjkzOX0.bhWFkJAMPAnEf9c1rRjEbyYG4XjQnOIP2dsVVeK_H3U";
 
-console.log("SUPABASE URL:", supabaseUrl);
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
