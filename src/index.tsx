825fc859e6647ae49f93d3c6c95b5295e9b7d48d
Ahmed Mohamed <162385026+AHMEDmoham3d@users.noreply.github.com>
2026-03-15 02:08:10 +0200

    test build version

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+console.log("BUILD VERSION 5");
 const root = ReactDOM.createRoot(
   document.getElementById('root')! // <-- استخدم "!" بدل "as HTMLElement"
 );
